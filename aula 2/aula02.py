f0551b64fffd83d250097b9fc2f32d84d6eee1b9
maridudaszz <eduardasousa2006ma@gmail.com>
2024-12-05 11:25:57 -0300

    aula02

---
+++ aula 2/aula02.py
@@ -0,0 +1,9 @@
+print('oi mundinho, sou a maria eduarda e adoro fazer pole')
+nome_usuario = 'maria duda' # tipo -> str = string
+idade = 30 # tipo -> int = integer
+altura = 1.62 # tipo -> float = flutuante
+maior_idade = True # tipo -> bool = boolean
+
+print ('Seja Bem-Vindo' , nome_usuario, 'sua idade é' , idade , 'anos', 'sua altura é', altura , 'você é maior de idade?' , maior_idade)
+
+
